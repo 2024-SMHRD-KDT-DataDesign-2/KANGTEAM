025ee532704e15ea434ac828358cea6a12cb22c0
LeeGH <miracle_ray@naver.com>
2024-12-21 09:05:50 +0900

    detailpage imageprint 2nd try

--- Data_At/src/main/webapp/resources/printJsonImages.js
+++ Data_At/src/main/webapp/resources/printJsonImages.js
@@ -1,15 +1,6 @@
 const fileSystemDiv = document.getElementById('file-system');
 const fileList = document.getElementById('file-list');
 
-// Add drag-and-drop events
-dropZone.addEventListener('dragover', (event) => {
-  event.preventDefault();
-  dropZone.classList.add('dragover');
-});
-
-dropZone.addEventListener('dragleave', () => {
-  dropZone.classList.remove('dragover');
-});
 
 // 페이지 로딩 시 json 이미지 가져오기
 $(document).ready(function () {
@@ -20,10 +11,10 @@ $(document).ready(function () {
         type: "POST",
         data: { "img_id": img_id },
         success: function (response) {
-            console.log("jsonImage:", response);
 
             // JSON 변환이 필요한 경우 처리
             const jsonImage = typeof response === "string" ? JSON.parse(response) : response;
+            console.log("jsonImage : ", jsonImage);
 
             // 응답 데이터가 배열인지 확인
             if (!Array.isArray(jsonImage)) {
